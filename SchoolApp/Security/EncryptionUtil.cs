f05e54202a8c1441d0bcafa02dcc8485a3e26cf3
panosrodo <brandonroots@hotmail.gr>
2025-10-31 11:39:35 +0200

    feat: add encryption util

---
+++ SchoolApp/Security/EncryptionUtil.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolApp.Security
+{
+    public static class EncryptionUtil
+    {
+        public static string Encrypt(string plainText)
+        {
+            var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(plainText);
+            return encryptedPassword;
+        }
+
+        public static bool IsValidPassword(string plaiText, string cipherText)
+        {
+            var isValid = BCrypt.Net.BCrypt.Verify(plaiText, cipherText);
+            return isValid;
+        }
+    }
+}
