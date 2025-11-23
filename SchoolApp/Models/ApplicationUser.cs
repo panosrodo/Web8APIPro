ce046dbfed6568589e47f7d2e024e6746894d2ec
panosrodo <brandonroots@hotmail.gr>
2025-11-23 19:15:51 +0200

    feat: add application user model

---
+++ SchoolApp/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolApp.Models
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
+    }
+}
