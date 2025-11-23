8fad23021affd774125a6eb0fbc46344e2687d0b
panosrodo <brandonroots@hotmail.gr>
2025-11-23 19:11:37 +0200

    feat: add base controller to fetch the principal

---
+++ SchoolApp/Controllers/BaseController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolApp.Models;
+using SchoolApp.Services;
+using System.Security.Claims;
+
+namespace SchoolApp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+
+        public readonly IApplicationService applicationService;
+
+        public BaseController(IApplicationService applicationService)
+        {
+            this.applicationService = applicationService;
+        }
+
+        private ApplicationUser? appUser;
+        protected ApplicationUser? AppUser
+        {
+            get
+            {
+                if (appUser != null && User.Claims != null && User.Claims.Any())
+                {
+                    var claimsType = User.Claims.Select(c => c.Type).ToList();
+                    if (!claimsType.Contains(ClaimTypes.NameIdentifier))
+                    {
+                        return null;
+                    }
+                    var userId = Convert.ToInt32(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+                    appUser = new ApplicationUser
+                    {
+                        Id = userId,
+                        Username = User.FindFirst(ClaimTypes.Name)?.Value,
+                        Email = User.FindFirst(ClaimTypes.Email)?.Value
+                    };
+                    return appUser;
+                }
+                return null;
+            }
+        }
+    }
+}
